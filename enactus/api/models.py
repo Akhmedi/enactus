b3d0f8ed6ada0123637c53f724e4aea46e28f205
Akhmedi <ak_orynbassar@kbtu.kz>
2025-08-31 13:17:24 +0500

    Add files via upload

---
+++ enactus/api/models.py
@@ -0,0 +1,26 @@
+from tastypie.resources import ModelResource
+from shop.models import Category, Course
+from tastypie.authorization import Authorization
+from .authentication import CustomAuthentication
+
+class CategoryResource(ModelResource):
+    class Meta:
+        queryset = Category.objects.all()
+        resourse_name = 'categories'
+        allowed_methods = ['get']
+
+class CourseResource(ModelResource):
+    class Meta:
+        queryset = Course.objects.all()
+        resourse_name = 'courses'
+        allowed_methods = ['get', 'post', 'delete']
+        authentication = CustomAuthentication()
+        authorization = Authorization()
+
+    def hydrate(self, bundle):
+        bundle.obj.category_id = bundle.data['category_id']
+        return bundle
+
+    def dehydrate(self, bundle):
+        bundle.data['category_id'] = bundle.obj.category
+        return bundle    
